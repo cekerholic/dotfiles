d329ef6634b15a54e309275019e534364ef8774b
Aris FM <aris@cekerholic.com>
2015-11-29 14:44:59 +0700

    Add chrome installation

--- program.sh
+++ program.sh
@@ -1,3 +1,9 @@
+# Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
+
 # Slack
 wget https://slack-ssb-updates.global.ssl.fastly.net/linux_releases/slack-desktop-1.2.5-amd64.deb
 sudo dpkg -i slack-desktop-1.2.5-amd64.deb
@@ -7,7 +13,7 @@ rm slack-desktop-1.2.5-amd64.deb
 sudo apt-get install -y unity-tweak-tool gnome-tweak-tool
 
 # Theme and Icons
-sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository -y ppa:noobslab/themes
 sudo apt-get update
 sudo apt-get install -y libra-theme
 sudo add-apt-repository ppa:numix/ppa
@@ -15,10 +21,9 @@ sudo apt-get update
 sudo apt-get install -y numix-icon-theme numix-icon-theme-circle
 
 # Variety wallpaper changer
-sudo add-apt-repository ppa:peterlevi/ppa
+sudo add-apt-repository -y ppa:peterlevi/ppa
 sudo apt-get update
 sudo apt-get install -y variety
 
 # htop
 sudo apt-get install -y htop
-
