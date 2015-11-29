ad3b22ad63c6e753cf49ea5dc7bcf06001e37084
Aris FM <aris@cekerholic.com>
2015-11-29 14:44:22 +0700

    Add postinstall file
    Rbenv ruby installation only works after restarting terminal, so I
    decided to separate the file. Oh, also essentials NPM packages as well

---
+++ post-install.sh
@@ -0,0 +1,10 @@
+# Essentials NPM Package
+npm install -g browser-sync gulp bower npm-check-updates http-server
+
+# Install ruby from rbenv
+rbenv install 2.2.2
+rbenv global 2.2.2
+gem install middleman
+
+# Disable online search
+gsettings set com.canonical.Unity.Lenses remote-content-search none
