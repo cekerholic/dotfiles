ada0bfc65d18ba2ed69b300874fd77a95860bef4
Aris FM <aris@cekerholic.com>
2015-11-29 14:40:36 +0700

    Add git install
    Including gitk and default .gitconfig & gitignore

--- init.sh
+++ init.sh
@@ -5,6 +5,16 @@ sudo apt-get install -y python-software-properties software-properties-common
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo apt-get update
 sudo apt-get install -y tmux
+ln -sfn $PWD/.tmux.conf ~/.tmux.conf
+
+# essentials
+sudo apt-get install -y build-essential libssl-dev curl
+
+# git
+sudo apt-get install -y git
+sudo apt-get install -y gitk
+ln -sfn $PWD/git/.gitconfig ~/.gitconfig
+ln -sfn $PWD/git/.gitignore ~/.gitignore
 
 # Install zsh
 sudo apt-get install -y zsh
@@ -12,7 +22,7 @@ sudo apt-get install -y zsh
 # Install oh-my-zsh
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 rm ~/.zshrc
-ln -sfn ~/.dotfiles/.zshrc ~/.zshrc
+ln -sfn $PWD/.zshrc ~/.zshrc
 
 # Set zsh as default
 chsh -s $(which zsh)
