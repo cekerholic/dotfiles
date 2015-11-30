7135632d4a7b43ffb504a240a31a41f46c91486a
Aris FM <aris@cekerholic.com>
2015-11-30 14:06:24 +0700

    Set terminal color to solarized

--- post-install.sh
+++ post-install.sh
@@ -8,3 +8,8 @@ gem install middleman
 
 # Disable online search
 gsettings set com.canonical.Unity.Lenses remote-content-search none
+
+# Change Terminal colors
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+cd gnome-terminal-colors-solarized
+sh ./set_dark.sh
