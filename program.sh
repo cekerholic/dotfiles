570f5b97db51b1692029ba8de1b0b22d41e69e04
Aris FM <aris@cekerholic.com>
2015-11-30 11:59:05 +0700

    Add Dropbox

--- program.sh
+++ program.sh
@@ -27,3 +27,8 @@ sudo apt-get install -y variety
 
 # htop
 sudo apt-get install -y htop
+
+# Dropbox
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E
+sudo add-apt-repository -y "deb http://linux.dropbox.com/ubuntu $(lsb_release -sc) main"
+sudo apt-get update && sudo apt-get install -y nautilus-dropbox
